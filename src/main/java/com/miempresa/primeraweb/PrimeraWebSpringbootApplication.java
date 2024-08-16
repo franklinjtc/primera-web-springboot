187443ce75bc66cb0a3be30ed193e477e8ada5a3
Franklin Tello <tellocardenasfranklinjunior@gmail.com>
2024-08-16 00:09:34 -0500

    capa controller, view

--- src/main/java/com/miempresa/primeraweb/PrimeraWebSpringbootApplication.java
+++ src/main/java/com/miempresa/primeraweb/PrimeraWebSpringbootApplication.java
@@ -1,10 +1,8 @@
 package com.miempresa.primeraweb;
 
-import com.miempresa.primeraweb.entities.Persona;
-import com.miempresa.primeraweb.repository.PersonaRepository;
+
 import com.miempresa.primeraweb.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,24 +10,24 @@ import java.sql.SQLOutput;
 import java.util.List;
 
 @SpringBootApplication
-public class PrimeraWebSpringbootApplication implements CommandLineRunner {
+public class PrimeraWebSpringbootApplication {
 	@Autowired
 	private PersonaService personaService;
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeraWebSpringbootApplication.class, args);
 	}
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
-		personaService.crearPersona(new Persona(6L,"Xiomara Gonzales",29));
-		personaService.crearPersona(new Persona(7L,"Rosario Cardenas",50));
-		personaService.crearPersona(new Persona(8L,"Enzo Tello",32));
-		personaService.crearPersona(new Persona(9L,"Gaetano Tello",8));
-		personaService.crearPersona(new Persona(10L,"Franklin Tello",34));
+		personaService.crearPersona(new Persona(11L,"Gonzales",29));
+		personaService.crearPersona(new Persona(12L,"Cardenas",50));
+		personaService.crearPersona(new Persona(13L,"Tello1",32));
+		personaService.crearPersona(new Persona(14L,"Tello2",8));
+		personaService.crearPersona(new Persona(15L,"Tello3",34));
 
 		System.out.println("Número de personas de la tabla: "+personaService.contarPersonas());
 
 		List<Persona> personas=personaService.obtenerTodas();
 		personas.forEach(p-> System.out.println("Nombre de persona : "+p.getNombre()));
-	}
+	}*/
 }
---
+++ src/main/java/com/miempresa/primeraweb/controller/PersonaController.java
@@ -0,0 +1,39 @@
+package com.miempresa.primeraweb.controller;
+
+import com.miempresa.primeraweb.entities.Persona;
+import com.miempresa.primeraweb.service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/personas")
+public class PersonaController {
+    @Autowired
+    private PersonaService personaService;
+    /*La clase Model se itiliza para transferir objetos del coontroller a la vista*/
+    @GetMapping
+    public String listarPersonas(Model model){
+        List <Persona> personas = personaService.obtenerTodas();
+        model.addAttribute("listaPersonas", personas);
+        return "listar";
+    }
+    @GetMapping("/nueva")
+    public String mostrarFormularioDeNuevaPersona(Model model){
+        model.addAttribute("persona", new Persona());
+        model.addAttribute("accion","/personas/nueva");
+        return "formulario";
+
+    }
+    @PostMapping("/nueva")
+    public String guardarNuevaPersona(@ModelAttribute Persona persona){
+        personaService.crearPersona(persona);
+        return "redirect:/personas";
+    }
+}
